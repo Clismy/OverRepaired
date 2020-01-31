26d1f9f87c99cc511b0bf08082a1c939834dc35c
Christos <christos.kalanidis@hotmail.com>
2020-01-31 23:52:07 +0100

    Player Movement

---
+++ Unity_OverRepaired/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    [SerializeField] float acceleration;
+    [SerializeField] float deAcceleration;
+
+    [SerializeField] bool secondPlayer;
+
+    Rigidbody rb;
+    Vector3 velocity;
+    Vector3 rotVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rotVelocity = Vector3.back;
+    }
+
+    void Update()
+    {
+        Move();
+        RotateTowardsVelocity();
+
+        if(rb.velocity != Vector3.zero)
+        {
+            rotVelocity = rb.velocity;
+        }
+    }
+
+
+    void Move()
+    {
+        float horizontal = secondPlayer == false ? Input.GetAxisRaw("Horizontal1") : Input.GetAxisRaw("Horizontal2");
+        float vertical =  secondPlayer == false ? Input.GetAxisRaw("Vertical1") : Input.GetAxisRaw("Vertical2");
+
+        velocity = SetVelocity(GetDirection(horizontal), GetDirection(vertical));
+
+        rb.velocity = velocity.normalized * movementSpeed;
+    }
+
+    void RotateTowardsVelocity()
+    {
+        transform.rotation = Quaternion.LookRotation(rotVelocity, Vector3.up);
+    }
+
+
+    Vector3 SetVelocity(float inputx, float inputy)
+    {
+        Vector3 input = new Vector3(inputx, 0f, inputy);
+
+        if(Mathf.Abs(input.sqrMagnitude) > 0)
+        {
+            
+        }
+        else
+        {
+
+        }
+
+
+        return input;
+    }
+
+
+    
+
+    int GetDirection(float input)
+    {
+        if(input > 0)
+        {
+            return 1;
+        }
+        else if(input < 0)
+        {
+            return -1;
+        }
+        return 0; 
+    }
+}
---
+++ Unity_OverRepaired/Assets/_Scripts/PlayerPickUp.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickUp : MonoBehaviour
+{
+    private bool pickedUp = false;
+    GameObject pickedUpGameobject;
+    [SerializeField] Transform pickUpPosition;
+
+    [SerializeField] float pickUpSpeed;
+
+    [SerializeField] float sphereRadius;
+    [SerializeField] LayerMask collideWithPickUp;
+    GameObject closestObject;
+    [SerializeField] bool secondPlayer = false;
+
+    void Update()
+    {
+        Collider[] sphereHits = Physics.OverlapSphere(transform.position, sphereRadius, collideWithPickUp);
+
+        Vector3 newTransform = transform.position;
+        float destination = Mathf.Infinity;
+
+        foreach(Collider r in sphereHits) // Find The Closest Object & Set It Into closestObject Gameobject
+        {
+            Vector3 diff = r.transform.position - newTransform;
+            float newDistance = diff.sqrMagnitude;
+
+            if(destination > newDistance && newDistance >= 0)
+            {
+                destination = newDistance;
+                closestObject = r.transform.gameObject;
+            }
+        }
+
+        if(closestObject != null) //We have found our closestObject
+        {
+            if(Time.time % 2  == 0)
+            {
+                Debug.Log("CLOSEST OBJECT IS " + closestObject.name);
+            }
+
+            string inputName = secondPlayer == false ? "PickUp1" : "PickUp2";
+            if(Input.GetButtonDown(inputName) && !pickedUp) //If we have nothing in hand, pick up
+            {
+                pickedUpGameobject = closestObject;
+                pickedUpGameobject.GetComponent<CapsuleCollider>().isTrigger = true;
+                pickedUpGameobject.GetComponent<Rigidbody>().isKinematic = true;
+                pickedUp = true;
+            }
+            else if(Input.GetButtonDown(inputName) && pickedUp) //If we have something in hand, throw
+            {
+                pickedUpGameobject.GetComponent<Rigidbody>().isKinematic = false;
+                pickedUpGameobject.GetComponent<Rigidbody>().AddForce(transform.forward * 15, ForceMode.Impulse);
+
+                pickedUpGameobject.GetComponent<CapsuleCollider>().isTrigger = false;
+                pickedUp = false;
+                pickedUpGameobject = null;
+            }
+        }
+
+        if (pickedUp)
+        {
+            pickedUpGameobject.transform.position = Vector3.MoveTowards(pickedUpGameobject.transform.position, pickUpPosition.position, Time.deltaTime * pickUpSpeed);
+            pickedUpGameobject.transform.rotation = Quaternion.RotateTowards(pickedUpGameobject.transform.rotation, pickUpPosition.rotation, Time.deltaTime * pickUpSpeed);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sphereRadius);
+    }
+}
